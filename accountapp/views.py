8c5e9618136f180a2bc5fd3352e507c2a0fd323b
Kimhyuntae9665 <rlagusxo96652@naver.com>
2023-07-05 14:47:45 +0900

    POST 를 이용한 HTTP 통신

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,7 +5,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    return  render(request,'accountapp/hello_world.html')
+    # 여기는 서버가 request를 받았을때의 처리를 담당하는 곳
+    #  서버가 request를 받았을때 render로 보여줄 페이지를 정하는 것
+    if request.method == "POST":
+        return  render(request,'accountapp/hello_world.html',context={'text2':'POST METHOD!!!'})
+    else :
+        return render(request,'accountapp/hello_world.html',context={'text2':'GET METHOD!!!'})
 
 
 
