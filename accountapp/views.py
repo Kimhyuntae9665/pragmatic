e385a874bb99557e00907dc7f4ecf5fe2409c1e9
Kimhyuntae9665 <rlagusxo96652@naver.com>
2023-07-08 17:48:38 +0900

    decorated ㅣㅇ용

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,33 +1,44 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
 
 # Create your views here.
 
+has_ownership = [account_ownership_reuired,login_required]
+
+@login_required
 def hello_world(request):
-    # 여기는 서버가 request를 받았을때의 처리를 담당하는 곳
-    #  서버가 request를 받았을때 render로 보여줄 페이지를 정하는 것
-    if request.method == "POST":
-        #      넘어온 request 요청에서 POST method를 사용해서 넘어온 요청에서 'hello_world_input'이라는 이름을 가진 데이터를 가져와라
-        temp = request.POST.get('hello_world_input')
 
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
-    
-        # reverse는 함수형 class 에서
-        return  HttpResponseRedirect(reverse('accountapp:hello_world'))
-    else :
 
-        hello_world_list = HelloWorld.objects.all()
-        return render(request,'accountapp/hello_world.html',context={'hello_world_list':hello_world_list})
+
+
+        # 여기는 서버가 request를 받았을때의 처리를 담당하는 곳
+        #  서버가 request를 받았을때 render로 보여줄 페이지를 정하는 것
+        if request.method == "POST":
+            #      넘어온 request 요청에서 POST method를 사용해서 넘어온 요청에서 'hello_world_input'이라는 이름을 가진 데이터를 가져와라
+            temp = request.POST.get('hello_world_input')
+
+            new_hello_world = HelloWorld()
+            new_hello_world.text = temp
+            new_hello_world.save()
+
+            # reverse는 함수형 class 에서
+            return  HttpResponseRedirect(reverse('accountapp:hello_world'))
+        else :
+
+            hello_world_list = HelloWorld.objects.all()
+            return render(request,'accountapp/hello_world.html',context={'hello_world_list':hello_world_list})
+
 
 
 
@@ -55,6 +66,8 @@ class AccountDetailView(DetailView):
 
 
 
+@method_decorator(has_ownership,'get')
+@method_decorator(has_ownership,'post')
 class AccountUpdateView(UpdateView):
     #  Django 에서 기본으로 제공해 주는 User model
     model = User
@@ -67,6 +80,8 @@ class AccountUpdateView(UpdateView):
     template_name='accountapp/update.html'
 
 
+@method_decorator(has_ownership,'get')
+@method_decorator(has_ownership,'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
