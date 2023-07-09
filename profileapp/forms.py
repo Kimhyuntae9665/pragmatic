ef9e5f4897e86e544cbfb7e71825136060d5df9d
Kimhyuntae9665 <rlagusxo96652@naver.com>
2023-07-09 22:11:21 +0900

    form class에서 보안 때문에 누락된 정보를 추가해 준다

--- profileapp/forms.py
+++ profileapp/forms.py
@@ -6,4 +6,5 @@ from profileapp.models import Profile
 class ProfileCreationForm(ModelForm):
     class Meta:
         model = Profile
+        #  fields 에 User 를 넣지 않은 이유는 field에 user 추가하면 user의 정보가 노출되면 보안 문제가 있어서
         fields = ['image','nickname','message']
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
 
 # Create your views here.
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/create.html'
+
+
+    #  form_class에서 field에서 user를 추가하지 않았다 이유는 보안문제 때문에 하지만 이러면 페이지가 요구하는 사항을 만족하지 못한다
+    #  그래서 VIew 논리를 구현하는 파일에서 user 필드도 추가해서 서버에 보내준다
+    def form_valid(self, form):
+        #  commit 데이터베이스에 저장하지는 않고 임시로 변수에 저장한다
+        temp_profile = form.save(commit = False)
+        #  user의 정보를 가져온다
+        temp_profile.user = self.request.user
+        #  form_class에 user의 정보를 더해서 데이터 베이스에 저장한다
+        temp_profile.save()
+        #  super인 조상 ProfileCreateView 여기에서 보내는 원래의 form_class 형태를 return 한다
+        return super().form_valid(form)
