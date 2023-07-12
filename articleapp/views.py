9e154229f7794eda942c29ee5aeefa5d59563c38
Kimhyuntae9665 <rlagusxo96652@naver.com>
2023-07-12 12:16:30 +0900

    list를 구축

--- articleapp/views.py
+++ articleapp/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
 
 from articleapp.decorators import article_ownership_required
 from articleapp.forms import ArticleCreationForm
@@ -48,14 +48,26 @@ class ArticleUpdateView(UpdateView):
 
 
 
-    def get_success_url(self):
+    def get_success_url(self):     # detail/<int:pk> 이기 때문데 detail에 들어가려면 pk가 꼭 필요하기 때문
         return reverse('articleapp:detail',kwargs={'pk':self.object.pk})
 
 
 
 
+
+@method_decorator(article_ownership_required,'get')
+@method_decorator(article_ownership_required,'post')
 class ArticleDeleteView(DeleteView):
     model = Article
     context_object_name = 'target_article'
     success_url = reverse_lazy('articleapp:list')
     template_name = 'articleapp/delete.html'
+
+
+
+
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'articleapp/list.html'
+    paginate_by = 5
