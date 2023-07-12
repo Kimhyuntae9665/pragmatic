6b7421caa2ad2fb3daa60d583bed12462ef21cba
Kimhyuntae9665 <rlagusxo96652@naver.com>
2023-07-12 19:47:56 +0900

    내가 원하는 형태를 새로운 페이지가 아니고 페이지 안에 페이지를 넣으려면 includeㅅ ㅏ용

--- pragmatic/urls.py
+++ pragmatic/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('accounts/',include('accountapp.urls')),
     path('profiles/',include('profileapp.urls')),
     path('articles/',include('articleapp.urls')),
+    path('comments/',include('commentapp.urls')),
 #     media에 관련한 추가적인 setting , pragmatic의 settings의 MEDIA_URL과 MEDIA_ROOT를 연결해서 서버에서 media 디렉토리에서 img파일을 가져온다
 ] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
