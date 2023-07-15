8634204d2b7811f96c4f9dd6ce1ee66bc9569f64
Kimhyuntae9665 <rlagusxo96652@naver.com>
2023-07-15 12:58:21 +0900

    extends base.html과 block content end block 을 모두 지워주고 다른 template 파일의 part로 들어간다

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -7,4 +7,4 @@ class ArticleCreationForm(ModelForm):
     class Meta:
         model = Article
         # witer는 보안상의 문제 때문에 제외 나중에 def form_valid로 추가할 것이다
-        fields=['title','image','content']
+        fields=['title','image','project','content']
